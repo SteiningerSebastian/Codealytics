6fb01f7e1debef2faadddada058946771cf08e68
Sebastian Steininger <34867906+SteiningerSebastian@users.noreply.github.com>
2022-01-14 11:24:11 +0100

    Add first HardwareMonitor (CPU-Usage)

--- Codealytics.Console/Program.cs
+++ Codealytics.Console/Program.cs
@@ -1,7 +1,8 @@
 ﻿using Codealytics;
+using Codealytics.HardwareMonitor;
 
 Analytics analytics = new Analytics();
-//analytics.HandleUi = true;
+analytics.HandleUi = true;
 ////Console.WriteLine(analytics.HandleUi);
 //analytics.AddMetric<string>("myString", "hello");
 //Thread.Sleep(1000);
@@ -28,21 +29,21 @@ Analytics analytics = new Analytics();
 //Thread.Sleep(1000);
 //analytics.Prefix = "Test\n";
 
-analytics.HandleUi = true;
-Random random = new Random();  
+//analytics.HandleUi = true;
+//Random random = new Random();  
 
-analytics.AddMetric<IRuntimePerformanceInfromation>("rpi", new RuntimePerformanceInfromation());
-Parallel.For(0, 100, (i) =>
-{
-    analytics.CodeRuntimePerformance("rpi", () =>
-    {
+//analytics.AddMetric<IRuntimePerformanceInfromation>("rpi", new RuntimePerformanceInfromation());
+//Parallel.For(0, 100, (i) =>
+//{
+//    analytics.CodeRuntimePerformance("rpi", () =>
+//    {
 
-        System.Threading.Thread.Sleep(random.Next(0, 1000));
+//        System.Threading.Thread.Sleep(random.Next(0, 1000));
 
-    });
-});
+//    });
+//});
 
-var rpi = analytics.GetMetric<IRuntimePerformanceInfromation>("rpi");
+//var rpi = analytics.GetMetric<IRuntimePerformanceInfromation>("rpi");
 
 //analytics.AddMetric<string>("varString", "World!");
 //Thread.Sleep(200);
@@ -85,3 +86,6 @@ var rpi = analytics.GetMetric<IRuntimePerformanceInfromation>("rpi");
 //    System.Threading.Thread.Sleep(100);
 //    analytics.CodeRuntimePerformance(id, () => { System.Threading.Thread.Sleep(900); });
 //}
+
+Console.WriteLine(Math.Round(HardwareMonitor.Instance.CPU).ToString() + "%");
+analytics.AddMetric<string>("CPU", () => { return Math.Round(HardwareMonitor.Instance.CPU).ToString() + "%"; });
---
+++ Codealytics.HardwareMonitor/HardwareMonitor.cs
@@ -0,0 +1,123 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Codealytics.HardwareMonitor
+{
+    public class HardwareMonitor
+    {
+        //Singelton
+        private static HardwareMonitor _instance = new HardwareMonitor();
+        private HardwareMonitor() {
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                throw new PlatformNotSupportedException();
+            }
+
+            //Start new thread that handels the PerformanceCounters
+            Thread thPerformanceCounter = new Thread(HandlePerformanceCounter);
+            thPerformanceCounter.Start();
+
+            Thread.Sleep(100);
+        }
+        public static HardwareMonitor Instance { get { return _instance; } }
+
+        /// <summary>
+        /// The delay between updates
+        /// </summary>
+        public int Delay { get; set; } = 300;
+
+        //All performanceCounters which handle cpu performance
+        private List<PerformanceCounter>? cpuPCounters = null;
+
+        private List<float> cpuAllCors_ = new List<float>();
+
+        /// <summary>
+        /// Returns a list containing the CPU-Usage of all cores, only supported on Windows.
+        /// </summary>
+        public List<float> CPUAllCores
+        {
+            get
+            {
+                lock (cpuAllCors_)
+                {
+                    return cpuAllCors_;
+                }
+            }
+            private set
+            {
+                lock (cpuAllCors_)
+                {
+                    cpuAllCors_ = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns a the CPU-Usage as avg of all cores, only supported on Windows.
+        /// </summary>
+        public float CPU
+        {
+            get
+            {
+                lock (CPUAllCores)
+                {
+                    return CPUAllCores.Sum() / CPUAllCores.Count;
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Handels all PerformanceCounter, shoul not be invoked by the main thread
+        /// </summary>
+        private void HandlePerformanceCounter()
+        {
+            while (true)
+            {
+                Thread.Sleep(Delay);
+                UpdateCPUUsage();
+            }
+        }
+
+        /// <summary>
+        /// Updates the CPUUsage
+        /// </summary>
+        /// <exception cref="PlatformNotSupportedException">Is thrown if the platform is not supported.</exception>
+        private void UpdateCPUUsage()
+        {
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                throw new PlatformNotSupportedException();
+            }
+
+            if (cpuPCounters == null || cpuPCounters.Count == 0)
+            {
+                var cat = new PerformanceCounterCategory("Processor Information");
+                char[] instances = cat.GetInstanceNames().Where(i => !i.Contains("Total")).Select(i => i[2]).ToArray();
+                cpuPCounters = new List<PerformanceCounter>();
+                for (int i = 0; i < Environment.ProcessorCount; i++)
+                {
+                    cpuPCounters.Add(new PerformanceCounter("Processor", "% Processor Time", $"{instances[i]}"));
+                }
+            }
+
+            List<float> cpuCores = new List<float>();
+            //Check if the platform is windows otherwise throw PlatformNotSupportedException
+
+            for (int i = 0; i < Environment.ProcessorCount; i++)
+            {
+                if (cpuCores.Count <= i)
+                {
+                    cpuCores.Add(cpuPCounters[i].NextValue());
+                }
+            }
+
+            lock (CPUAllCores)
+            {
+                CPUAllCores.Clear();
+                CPUAllCores.AddRange(cpuCores);
+            }
+        }
+    }
+}
